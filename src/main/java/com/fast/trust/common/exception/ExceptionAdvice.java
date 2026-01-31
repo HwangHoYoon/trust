5da090660065c71c33e630154e82dda530829e62
황호윤 <hoyoon9227@gmail.com>
2026-01-31 21:54:28 +0900

    exception 설정 제거

--- src/main/java/com/fast/trust/common/exception/ExceptionAdvice.java
+++ src/main/java/com/fast/trust/common/exception/ExceptionAdvice.java
@@ -3,10 +3,8 @@ package com.fast.trust.common.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
 public class ExceptionAdvice {
 
     //    @ExceptionHandler(value = {NoUserExistException.class, WrongPasswordException.class})
